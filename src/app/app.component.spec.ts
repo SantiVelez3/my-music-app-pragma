71a2d837c12b2a088d102a6a35ef6d4ab43a7fa3
santiago.velez3 <santiago.velez@pragma.com.co>
2022-02-08 10:23:58 -0500

    files upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-music-app-pragma');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-music-app-pragma app is running!');
-  });
+
 });
